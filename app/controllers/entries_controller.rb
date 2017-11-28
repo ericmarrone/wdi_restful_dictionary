b770e0eb28a463a11520a41fb8260e6dadf3888d
Eric Marrone <emarrone17@gmail.com>
2017-11-28 03:47:12 -0500

    Add controller actions, views, partial views for CRUD actions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,34 +1,55 @@
 class EntriesController < ApplicationController
   def index
+    @entries = Entry.all
     render :index
   end
 
   def show
-
+    @entry = Entry.find(params[:id])
   end
   def end
 
   end
 
   def new
+    @entry = Entry.new
     render :new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to new_entry_url
+    end
 
   end
 
   def edit
+    @entry = Entry.find(params[:id])
     render :edit
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to entry_url(@entry)
+    end
   end
 
   def destroy
-    redirect_to :index
-
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
 end
